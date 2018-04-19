a4d0dc235f9981fcc08a1da4ff57b30cdcadb15d
Greg Meyer <gregory.meyer@gmail.com>
2018-04-19 16:21:19 -0700

    fix test output to multiple files when running in parallel

--- test-main.cpp
+++ test-main.cpp
@@ -2,7 +2,7 @@
  *  This file is part of GASMat
  *  (C) Greg Meyer, 2018
  */
- 
+
 #include <upcxx/upcxx.hpp>
 #include <string>
 
@@ -14,14 +14,19 @@ int main( int argc, char* argv[] ) {
   upcxx::init();
 
   /* if we have multiple ranks, set up stdout to go to various files */
+  std::ofstream out;
   if (upcxx::rank_n() > 1) {
     std::string fname = "test_out_" + std::to_string(upcxx::rank_me()) + ".txt";
-    std::ofstream out(fname);
+    out.open(fname);
     std::cout.rdbuf(out.rdbuf());
   }
 
   /* actually run catch */
   int result = Catch::Session().run( argc, argv );
+
+  if (upcxx::rank_n() > 1) {
+    out.close();
+  }
 
   upcxx::finalize();
 
